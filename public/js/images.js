176727e7b475404b17df94f2bbdeaaeb17c5ffc0
Hayden Supple <hsupple@purdue.edu>
2026-03-09 21:50:47 -0400

    Added images

---
+++ public/js/images.js
@@ -0,0 +1,16 @@
+/**
+ * Universal image paths for Supple Automotive.
+ * Use these names in HTML/CSS when wiring up images.
+ * Place image files in public/images/ with these exact filenames.
+ */
+window.SUPPLE_IMAGES = {
+  hero: 'images/hero.jpg',
+  processSchedule: 'images/process-schedule.jpg',
+  processDiagnose: 'images/process-diagnose.jpg',
+  processPartsQuote: 'images/process-parts-quote.jpg',
+  processRepairGo: 'images/process-repair-go.jpg',
+  serviceMaintenance: 'images/service-maintenance.jpg',
+  serviceDiagnostics: 'images/service-diagnostics.jpg',
+  serviceInspection: 'images/service-inspection.jpg',
+  aboutHayden: 'images/about-hayden.jpg'
+};
