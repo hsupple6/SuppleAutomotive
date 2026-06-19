3177e7ba68a76786ae9d95305f4a1b6d1d386c92
Hayden Supple <hsupple@purdue.edu>
2026-06-18 20:50:13 -0400

    IMAGES

--- public/js/images.js
+++ public/js/images.js
@@ -5,6 +5,7 @@
  */
 window.SUPPLE_IMAGES = {
   hero: 'images/hero.jpg',
+  heroAlt: 'images/hero1.jpg',
   processSchedule: 'images/process-schedule.jpg',
   processDiagnose: 'images/process-diagnose.jpg',
   processPartsQuote: 'images/process-parts-quote.jpg',
@@ -12,5 +13,21 @@ window.SUPPLE_IMAGES = {
   serviceMaintenance: 'images/service-maintenance.jpg',
   serviceDiagnostics: 'images/service-diagnostics.jpg',
   serviceInspection: 'images/service-inspection.jpg',
-  aboutHayden: 'images/mechanic.jpg'
+  aboutHayden: 'images/mechanic.jpg',
+  oil1: 'images/Oil1.jpg',
+  oil2: 'images/Oil2.jpg',
+  oil3: 'images/Oil3.png',
+  oilFilter1: 'images/OilFilter1.webp',
+  oilFilter2: 'images/OilFilter2.jpg',
+  oilFilter3: 'images/OilFilter3.webp',
+  battery1: 'images/Battery1.webp',
+  battery2: 'images/Battery2.jpg',
+  alternator1: 'images/Alternator1.jpg',
+  alternator2: 'images/Alternator2.PNG',
+  brake1: 'images/Brake1.jpg',
+  brake2: 'images/Brake2.jpeg',
+  brakePad1: 'images/BrakePad1.webp',
+  suspension1: 'images/Suspension1.jpg',
+  suspension2: 'images/Suspension2.jpg',
+  suspension3: 'images/Suspension3.jpeg'
 };
